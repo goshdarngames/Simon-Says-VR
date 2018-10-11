d0523acd18d1b09253d82215e619222633f8067e
None <fake@fake.com>
2018-10-11 17:17:32 +0100

    Added renderLoop funciton definition.

--- game_logic/renderLoop.js
+++ game_logic/renderLoop.js
@@ -4,3 +4,12 @@
  * This is the main game loop that is used to update objects and render
  * the active scene.
  ***************************************************************************/
+
+( function ( babylonProject, undefined )
+{
+
+    babylonProject.renderLoop = function ()
+    {
+    };
+
+} ( window.babylonProject = window.babylonProject || {} ));
