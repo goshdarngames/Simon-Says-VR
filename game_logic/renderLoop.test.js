47b160b3ce43efd0ff3dbd2d8c0e91d1cf9e243b
None <fake@fake.com>
2018-10-11 17:28:51 +0100

    Added activeScene render test and corrected mistake in definition test.

--- game_logic/renderLoop.test.js
+++ game_logic/renderLoop.test.js
@@ -4,6 +4,13 @@ const renderLoop = require ( "./renderLoop" );
  * MOCK DATA
  ***************************************************************************/
 
+beforeEach ( () =>
+{
+    window.babylonProject.activeScene = jest.fn ();
+
+    window.babylonProject.activeScene.render = jest.fn ();
+});
+
 /****************************************************************************
  * TESTS
  ***************************************************************************/
@@ -12,6 +19,14 @@ describe ( "window.babylonProject.renderLoop", () =>
 {
     test ( "is defined", () =>
     {
-        expect ( window.babylonProject ).toBeDefined ();
+        expect ( window.babylonProject.renderLoop ).toBeDefined ();
+    });
+
+    test ( "calls window.babylonProject.activeScene.render", () =>
+    {
+        window.babylonProject.renderLoop ();
+
+        expect ( window.babylonProject.activeScene.render )
+            .toHaveBeenCalledTimes ( 1 );
     });
 });
