7fc111e8da9779e004bc5703716d2fe998bdf99b
None <fake@fake.com>
2018-10-11 16:34:54 +0100

    Created render loop files.

---
+++ game_logic/renderLoop.js
@@ -0,0 +1,6 @@
+/****************************************************************************
+ * renderLoop.js
+ *
+ * This is the main game loop that is used to update objects and render
+ * the active scene.
+ ***************************************************************************/
