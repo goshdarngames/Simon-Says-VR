ee5133d2731429c6120352b50e6b8c8a6041e707
None <fake@fake.com>
2018-10-12 15:36:00 +0100

    Added createVRScene definition.

---
+++ game_logic/createVRScene.js
@@ -0,0 +1,10 @@
+/****************************************************************************
+ * createVRScene.js
+ ***************************************************************************/
+
+( function ( babylonProject, undefined )
+{
+    babylonProject.createVRScene = function ()
+    {
+    };
+} ( window.babylonProject = window.babylonProject || {} ));
