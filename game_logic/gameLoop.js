8ef152bb568b14cbd5362815605630413b9d4171
None <fake@fake.com>
2018-10-17 16:31:25 +0100

    Updated file header comment.

--- game_logic/gameLoop.js
+++ game_logic/gameLoop.js
@@ -1,8 +1,13 @@
 /****************************************************************************
- * renderLoop.js
+ * gameLoop.js
  *
  * This is the main game loop that is used to update objects and render
- * the active scene.
+ * the scene.
+ *
+ * A finite state machine is used to switch between logical scenarios
+ * such as the main menu or gameplay.
+ *
+ * The states are defined as objects in ./game_state
  ***************************************************************************/
 
 ( function ( babylonProject, undefined )
