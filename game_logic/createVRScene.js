3d95e848a6e62aa2e1c6faac0db3666fce9ed715
None <fake@fake.com>
2018-11-30 12:28:01 +0000

    Documentation.

--- game_logic/createVRScene.js
+++ game_logic/createVRScene.js
@@ -1,9 +1,16 @@
 /****************************************************************************
  * createVRScene.js
+ *
+ * Used to create an empty babylon scene with the default VR experience.
  ***************************************************************************/
 
 ( function ( babylonProject, undefined )
 {
+    /**
+     * createVRSceme ( babylon, engine )
+     *
+     * Returns a Babylon scene with the default VR experience.     
+     */
     babylonProject.createVRScene = function ( babylon, engine )
     {
         let scene = new babylon.Scene ( engine );
