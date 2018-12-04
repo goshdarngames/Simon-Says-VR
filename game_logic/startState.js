abd31221940fe908374b96de5f5bd2a3cf808aa8
None <fake@fake.com>
2018-12-04 14:27:49 +0000

    Added header

--- game_logic/startState.js
+++ game_logic/startState.js
@@ -1,6 +1,18 @@
+/****************************************************************************
+ * startState.js
+ *
+ * When the page is loaded the initial state that the game loop will
+ * enter into will be babylonProject.StartState
+ *
+ * The StartState object should have an update function that returns the
+ * next state.  
+ ***************************************************************************/
 ( function ( babylonProject,  undefined )
 {
-
+    /**
+     * This object will be passed to the game loop as the first state
+     * when the page is loaded.
+     */
     babylonProject.StartState = function ( babylonScene )
     {
         if ( babylonScene == undefined )
